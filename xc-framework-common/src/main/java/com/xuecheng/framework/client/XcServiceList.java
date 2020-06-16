64e42e9b903f31833ec4e8373317b17bfa643e9f
YinHaoQiu <645183118@qq.com>
2020-06-17 02:15:43 +0800

    更新

--- xc-framework-common/src/main/java/com/xuecheng/framework/client/XcServiceList.java
+++ xc-framework-common/src/main/java/com/xuecheng/framework/client/XcServiceList.java
@@ -16,5 +16,5 @@ public class XcServiceList {
     public static final String XC_SERVICE_BASE_ID = "xc-service-base-id";
     public static final String XC_SERVICE_MANAGE_ORDER = "xc-service-manage-order";
     public static final String XC_SERVICE_LEARNING = "xc-service-learning";
-
+   public    String tes;
 }
